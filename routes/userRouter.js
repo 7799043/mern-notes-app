299577b64d731123fb81538030b20b035035fba1
7799043 <leoniak.maciek1@gmail.com>
2023-06-09 21:40:18 +0200

    .

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const userCtrl = require('../controllers/userCtrl')
-// const auth = require('../middleware/auth')
+const auth = require('../middleware/auth')
 
 // Register User
 
@@ -10,6 +10,6 @@ router.post('/register', userCtrl.registerUser)
 router.post('/login', userCtrl.loginUser)
 
 // // verify Token
-// router.get('/verify', userCtrl.verifiedToken)
+router.get('/verify', userCtrl.verifiedToken)
 
 module.exports = router
