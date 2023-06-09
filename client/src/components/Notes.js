7d19d5f09a619cc7ca4232798ace9db882d28ce0
7799043 <leoniak.maciek1@gmail.com>
2023-06-09 11:29:42 +0200

    small steps

---
+++ client/src/components/Notes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function Notes() {
+
+    return (
+       <section className="notes-page">
+           <div className="notes create-note">
+                <h2>Notes</h2>
+               </div>
+       </section>
+    )
+}
