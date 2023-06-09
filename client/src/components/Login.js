74ad1c922a59985e2e1d5a8eb3133c05e774297b
7799043 <leoniak.maciek1@gmail.com>
2023-06-09 11:56:45 +0200

    .

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,10 +4,23 @@ import React from 'react'
 export default function Login() {
 
     return (
-       <section className="login-page">
-           <div className="login create-note">
+        <section className="login-page">
+            <div className="login create-note">
                 <h2>Login</h2>
-               </div>
-       </section>
+                <form>
+                    <input type="email" name="email" id="login-email"
+                        placeholder="Email" />
+
+                    <input type="password" name="password" id="login-password"
+                        placeholder="Password" autoComplete="true" />
+
+                    <button type="submit">Login</button>
+                    <p>You don't have an account?
+                        <span> Register Now</span>
+                    </p>
+                    <h3></h3>
+                </form>
+            </div>
+        </section>
     )
 }
