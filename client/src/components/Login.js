ec7932b198200cda72efb899ced9923bc2bd24a6
7799043 <leoniak.maciek1@gmail.com>
2023-06-09 11:29:53 +0200

    small steps

---
+++ client/src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function Login() {
+
+    return (
+       <section className="login-page">
+           <div className="login create-note">
+                <h2>Login</h2>
+               </div>
+       </section>
+    )
+}
