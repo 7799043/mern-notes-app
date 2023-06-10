103e18bcccde73888ee40c9d3d0ed1cbea6fd495
7799043 <leoniak.maciek1@gmail.com>
2023-06-10 21:07:30 +0200

    .

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,32 @@ import Notes from './components/Notes'
 function App() {
   const [isLogin, setIsLogin] = useState(false)
 
-return (
-  <div className='App'>
-{
-  isLogin ? < Notes /> : < Login/>
-}
-  </div>
-);
-}
+  useEffect(() =>{
+    const checkLogin = async () =>{
+      const token = localStorage.getItem('tokenStore')
+      if(token){
+        const verified = await axios.get('/users/verify',{
+          headers:{ Authorization: token}
+        })
+        console.log(verified)
+        setIsLogin(verified.data)
+        if(verified.data === false) return localStorage.clear()
+      }else{
+        setIsLogin(false)
+      }
+    }
+    checkLogin()
+  }, [])
 
+  return (
+    <div className="App">
+      {
+        isLogin 
+        ? <Notes setIsLogin={setIsLogin} /> 
+        : <Login setIsLogin={setIsLogin} />
+      }
+    </div>
+  );
+}
 
 export default App;
