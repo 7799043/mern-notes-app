51ff4842ec2db4d34a2ac62a0e1d181380be599c
7799043 <leoniak.maciek1@gmail.com>
2023-06-20 13:47:13 +0200

    token test

--- tests/userController/userController-verifiedToken.test.js
+++ tests/userController/userController-verifiedToken.test.js
@@ -1,58 +1,38 @@
 const request = require('supertest');
 const app = require('../../server');
-const userCtrl = require('../../controllers/userCtrl');
-const Users = require('../../models/userModel');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
-describe('GET /verifiedToken', () => {
+
+
+
+
+describe('API Endpoint - verifiedToken', () => {
   it('should return true if token is valid', async () => {
-    const user = {
-      _id: 'testUserId',
-      username: 'testuser',
-    };
-
-    // Generowanie ważnego tokenu JWT
-    const token = jwt.sign({ id: user._id, name: user.username }, process.env.TOKEN_SECRET, {
-      expiresIn: '1d',
-    });
+    const validToken = '...'; 
 
     const response = await request(app)
-      .get('/verifiedToken')
-      .set('Authorization', token);
+      .get('/users/verify')
+      .set('Authorization', validToken);
 
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('true');
-  });
-
-  it('should return false if token is missing', async () => {
-    const response = await request(app).get('/verifiedToken');
-
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('false');
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(true);
   });
 
   it('should return false if token is invalid', async () => {
-    const invalidToken = 'invalidtoken';
+    const invalidToken = 'invalidToken'; 
 
     const response = await request(app)
-      .get('/verifiedToken')
+      .get('/users/verify')
       .set('Authorization', invalidToken);
 
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('false');
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(false);
   });
 
-  it('should return false if user does not exist', async () => {
-    const userToken = jwt.sign({ id: 'nonexistentUserId', name: 'nonexistentuser' }, process.env.TOKEN_SECRET, {
-      expiresIn: '1d',
-    });
-
+  it('should return false if no token is provided', async () => {
     const response = await request(app)
-      .get('/verifiedToken')
-      .set('Authorization', userToken);
+      .get('/users/verify');
 
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('false');
+    expect(response.status).toBe(200);
+    expect(response.body).toBe(false);
   });
 });
