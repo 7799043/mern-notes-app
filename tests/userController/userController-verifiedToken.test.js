c44029fe9cca8f7c525797d406205b839ee92f57
7799043 <leoniak.maciek1@gmail.com>
2023-06-20 13:56:11 +0200

    .

--- tests/userController/userController-verifiedToken.test.js
+++ tests/userController/userController-verifiedToken.test.js
@@ -1,29 +1,23 @@
 const request = require('supertest');
 const app = require('../../server');
 
-const jwt = require('jsonwebtoken');
 
-const generateTestToken = () => {
-  const payload = { id: 'testUserId', name: 'Test User' };
-  const secret = 'smacznejkawusi007'; 
-  const options = { expiresIn: '1d' };
-
-  const token = jwt.sign(payload, secret, options);
-  return token;
-};
 
 describe('API Endpoint - verifiedToken', () => {
   it('should return true if token is valid', async () => {
-    const validToken = generateTestToken();
-
+    const loginResponse = await request(app)
+      .post('/users/login')
+      .send({ email: 'test@example.com', password: 'testpassword' });
+  
+    const validToken = loginResponse.body.token;
+  
     const response = await request(app)
       .get('/users/verify')
       .set('Authorization', validToken);
-
+  
     expect(response.status).toBe(200);
     expect(response.body).toBe(true);
   });
-
   it('should return false if token is invalid', async () => {
     const invalidToken = 'invalidToken'; 
 
